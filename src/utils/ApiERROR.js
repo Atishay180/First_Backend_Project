43510594753517a2b2f0e6cc2f6b8186802ac1a4
Atishay180 <atishayjain8807@gmail.com>
2024-07-08 20:14:42 +0530

    little but study of middlewares

---
+++ src/utils/ApiERROR.js
@@ -0,0 +1,23 @@
+class ApiERROR extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stack = ""
+    ) {  //overrude
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false
+        this.errors = errors
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export { ApiERROR }
